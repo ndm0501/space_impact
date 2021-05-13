320cfeb20b88d93a9fca86105683b09b96c29d38
Mohammad Nadeem <mohammadnadeem@ip-192-168-1-6.ap-south-1.compute.internal>
2021-05-13 15:49:32 +0530

    adding and positioning the spacecraft

--- js/index.js
+++ js/index.js
@@ -1,8 +1,35 @@
-console.log("JS integrated!")
-const canvas = document.querySelector('canvas');
+
+const canvas = document.querySelector("canvas");
+
+//create canvas context
+const ctx = canvas.getContext("2d");
 
 //setting canvas width & height
 canvas.width = innerWidth;
-canvas.height= innerHeight;
+canvas.height = innerHeight;
 
-console.log(canvas)
+//creating spacecraft bluprint
+class SpaceCraft {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+  create() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill()
+  }
+}
+
+//positionaing the aircraft
+const x = canvas.width / 2;
+const y = canvas.height * 0.9;
+
+//creating soacecraft
+const spaceCraft = new SpaceCraft(x, y, 20, 'blue');
+spaceCraft.create()
+
+
