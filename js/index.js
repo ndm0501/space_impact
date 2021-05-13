554aeec3a01a327fda6ec18e4c588a3c146dfd2f
Mohammad Nadeem <mohammadnadeem@ip-192-168-1-6.ap-south-1.compute.internal>
2021-05-13 14:18:19 +0530

    bare minimum setup

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("JS integrated!")
+const canvas = document.querySelector('canvas');
+
+//setting canvas width & height
+canvas.width = innerWidth;
+canvas.height= innerHeight;
+
+console.log(canvas)
